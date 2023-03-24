b3896510ee89f2efd5e4dc1448417d7a85549b0d
Zokir Sobirjonov <zokir5857@gmail.com>
2023-03-24 17:35:29 +0500

    add GetCompany by Id

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -2,6 +2,7 @@
 using Contracts;
 using Service.Contracts;
 using Shared.DataTransferObjects;
+using System.Diagnostics.SymbolStore;
 
 namespace Service
 {
@@ -23,6 +24,14 @@ namespace Service
                 var companies = _repositoryManager.Company.GetAllCompanies(trackChanges);
                 var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
                 return companiesDto;
+        }
+
+        public CompanyDto GetCompany(Guid id, bool trackChanges)
+        {
+            var company = _repositoryManager.Company.GetCompany(id, trackChanges);
+
+            var companyDto = _mapper.Map<CompanyDto>(company);
+            return companyDto;
         }
     }
 }
