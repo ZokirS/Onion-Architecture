490b9e629a9b7ebdcf9b6f39c4f2f9039f0f7c8e
Zokir Sobirjonov <zokir5857@gmail.com>
2023-03-26 13:52:08 +0500

    add XML support as response body

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -13,8 +13,13 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
-builder.Services.AddControllers()
-    .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters()
+  .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+
 builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
