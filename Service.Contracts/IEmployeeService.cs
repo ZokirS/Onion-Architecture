10f5c8a8dfe468215f8e319291e310c49dbf48fd
ZokirS <zokir5857@gmail.com>
2023-03-27 17:48:53 +0500

    Add Put method to update employee

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -8,5 +8,6 @@ namespace Service.Contracts
         EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges);
         EmployeeDto CreateEmployee(Guid companyId, EmployeeForCreationDto employee, bool trackChanges);
         void DeleteEmployeeForCompany(Guid companyId, Guid id, bool trackChanges);
+        void UpdateEmployeeForCompany(Guid companyId, Guid id, EmployeeForUpdateDto employeeForUpdate, bool compTrackChanges, bool empTrackChanges);
     }
 }
