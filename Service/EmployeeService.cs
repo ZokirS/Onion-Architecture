f07dbe575e89d2acf1b238f554a84469ee6164ce
ZokirS <zokir5857@gmail.com>
2023-04-05 16:26:16 +0500

    add DataShaping for Employee with selecting which field will show as result to client

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -5,6 +5,7 @@ using Entities.Models;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 using Shared.RequestFeatures;
+using System.Dynamic;
 
 namespace Service
 {
@@ -13,14 +14,17 @@ namespace Service
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
         private readonly IMapper _mapper;
-        public EmployeeService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+        private readonly IDataShaper<EmployeeDto> _dataSharper;
+        public EmployeeService(IRepositoryManager repository, ILoggerManager logger, 
+            IMapper mapper, IDataShaper<EmployeeDto> dataSharper)
         {
             _repository = repository;
             _logger = logger;
             _mapper = mapper;
+            _dataSharper = dataSharper;
         }
 
-        public async Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId,
+        public async Task<(IEnumerable<ExpandoObject> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId,
             EmployeeParameters employeeParameters, bool trackChanges)
         {
             if (!employeeParameters.ValidAgeRange)
@@ -31,7 +35,9 @@ namespace Service
             var employeesWithMetaData = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges);
             var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesWithMetaData);
 
-            return (employees: employeesDto, metaData: employeesWithMetaData.MetaData);
+            var shapedData = _dataSharper.ShapeData(employeesDto, employeeParameters.Fields);
+
+            return (employees: shapedData, metaData: employeesWithMetaData.MetaData);
         }
 
         public async Task<EmployeeDto> GetEmployeeAsync(Guid companyId,Guid id, bool trackChanges)       
